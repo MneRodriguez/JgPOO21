956a450aff45a2af32b8088515be97302cb889e3
Mariano Nicolas Ezequiel Rodriguez <mnerodriguez@gmail.com>
2021-10-12 18:09:11 -0300

    Initial commit

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Proyecto26;
+using System.Linq;
+
+[Serializable]
+
+public class Jugador
+{
+    public string nombreJugador;
+    public int scoreLogradoPorJugador;
+
+    public Jugador()
+    {
+        nombreJugador =
+        scoreLogradoPorJugador =
+    }
+}
